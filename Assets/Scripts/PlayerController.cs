4182e22a2f3d37257e96c816cdca3c113af9b66c
Emanuele Villani <manu.lebon@hotmail.it>
2022-02-26 18:59:08 +0100

    Jump
    jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,12 @@ public class PlayerController : MonoBehaviour
     public CharacterController controller;
     private Vector3 direction;
     public float speed = 8f;
+    public float jumpForce = 7;
+    public  float gravity = -25;
+    public Transform grounCheck;
+    public LayerMask groundLayer;
+    public bool ableToMakeDoubleJump = true;
+
     void Start()
     {
         
@@ -16,9 +22,36 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         float hInput = Input.GetAxis("Horizontal");
         direction.x = hInput * speed;
         controller.Move(direction*Time.deltaTime);
-        
+        direction.y += gravity * Time.deltaTime;
+        bool isGrounded = Physics.CheckSphere(grounCheck.position,0.15f,groundLayer);
+
+
+        if (isGrounded)
+        {
+            ableToMakeDoubleJump = true;
+            if (Input.GetButtonDown("Jump"))
+            {
+                direction.y = jumpForce;
+            }
+            else
+
+            {
+                if (ableToMakeDoubleJump & Input.GetButtonDown("Jump"))
+                {
+                    Debug.Log("DoubleJump");
+
+                    direction.y = jumpForce;
+                    ableToMakeDoubleJump = false;
+                }
+                   
+
+                
+            }
+        }
+        controller.Move(direction * Time.deltaTime);
     }
 }
